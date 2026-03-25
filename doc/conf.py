b50fe40fa51a2de1e2e43516f5d4c3bea89ca60f
ullu37 <70131659+ullu37@users.noreply.github.com>
2026-03-25 11:36:19 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,11 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser', "autodoc2"]
+
+autodoc2_packages = [
+    "multiply.py"
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
