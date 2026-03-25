b24eb521f28e258e24aa603bc63c34779bf50cca
ullu37 <70131659+ullu37@users.noreply.github.com>
2026-03-25 11:34:38 +0100

    Create multiply.py

---
+++ src/multiply.py
@@ -0,0 +1,9 @@
+def multiply(a: float, b: float) -> float:
+    """
+    Multiply two numbers.
+
+    :param a: First number.
+    :param b: Second number.
+    :return: The product of a and b.
+    """
+    return a * b
